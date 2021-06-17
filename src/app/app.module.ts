3d256afcd61045e4591be58e8bbbd59a7619eb32
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-06-18 00:48:59 +0300

    signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/signup-form/signup-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, } from '@angular/core';
+import { NgForm, } from '@angular/forms';
+
+import { UserModel, } from '../../models/user.model';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: [
+    './signup-form.component.scss',
+  ],
+})
+export class SignupFormComponent implements OnInit {
+  public contries = [ 'Ukraine', 'Armenia', 'Belarus',
+                      'Hungry', 'Kazakhstan', 'Poland', 'Russia', ];
+
+  public user = new UserModel();
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+  public onSave(signupForm: NgForm): void {
+    console.log(signupForm.form);
+    console.log(`Saved: ${JSON.stringify(signupForm.value)}`);
+  }
+}
