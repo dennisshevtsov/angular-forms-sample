9ccff8d267c76be046808d242095647634e32247
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-06-23 01:55:47 +0300

    reactive form

--- src/app/components/signup-reactive-form/signup-reactive-form.component.ts
+++ src/app/components/signup-reactive-form/signup-reactive-form.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit, } from '@angular/core';
+import { FormControl, FormGroup, } from '@angular/forms';
+
+import { UserModel, } from '../../models/user.model';
 
 @Component({
   templateUrl: './signup-reactive-form.component.html',
   styleUrls: ['./signup-reactive-form.component.scss']
 })
 export class SignupReactiveFormComponent implements OnInit {
+  public countries = [
+    'Armenia', 'Belarus', 'Hungry', 'Kazakhstan',
+    'Poland', 'Russia', 'Ukraine', ];
+  public user = new UserModel();
+  public userForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.initializeForm();
   }
 
+  public initializeForm(): void {
+    this.userForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendProducts: new FormControl(true),
+    });
+  }
+
+  public onSave(): void {
+    console.log(this.userForm);
+    console.log(`Saved: ${JSON.stringify(this.userForm.value)}`);
+    console.log(`Saved: ${JSON.stringify(this.userForm.getRawValue())}`);
+  }
 }
