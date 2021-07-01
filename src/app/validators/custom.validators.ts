a078cf1d4ea6f54f31fa80e5feae26c0ee4d7861
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-01 10:51:54 +0300

    add log

--- src/app/validators/custom.validators.ts
+++ src/app/validators/custom.validators.ts
@@ -2,6 +2,8 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class CustomValidators {
   static serviceLevel(c: AbstractControl): ValidationErrors | null {
+    console.log('Validator: serviceLevel is called');
+
     if (c.value !== undefined && (Number.isNaN(c.value) || c.value < 1 || c.value > 5)) {
       return {
         serviceLevel: true,
