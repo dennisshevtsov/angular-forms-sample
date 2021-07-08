723b4b7e55146172656e2e8e87e49332de2b2c33
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-08 16:09:22 +0300

    async email validator

---
+++ src/app/validators/async-email-validator.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAsyncEmailValidator]'
+})
+export class AsyncEmailValidatorDirective {
+
+  constructor() { }
+
+}
--- src/app/validators/validators.module.ts
+++ src/app/validators/validators.module.ts
@@ -1,16 +1,19 @@
 import { NgModule, } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { ServiceLevelDirective } from './service-level.directive';
+import { AsyncEmailValidatorDirective } from './async-email-validator.directive';
 
 @NgModule({
   declarations: [
-    ServiceLevelDirective
+    ServiceLevelDirective,
+    AsyncEmailValidatorDirective
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    ServiceLevelDirective
+    ServiceLevelDirective,
+    AsyncEmailValidatorDirective
   ],
 })
 export class ValidatorsModule { }
