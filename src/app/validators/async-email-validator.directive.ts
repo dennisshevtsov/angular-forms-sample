12ff6fdae98ee3d891631c5fb8292cc4c7b560d9
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-08 16:12:21 +0300

    async email valid

--- src/app/validators/async-email-validator.directive.ts
+++ src/app/validators/async-email-validator.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive,           } from '@angular/core';
+import { NG_ASYNC_VALIDATORS, } from '@angular/forms';
 
 @Directive({
-  selector: '[appAsyncEmailValidator]'
+  selector: '[appAsyncEmailValidator][formControlName],[appAsyncEmailValidator][ngModel]',
+  providers: [
+    {
+      provide: NG_ASYNC_VALIDATORS,
+      useExisting: AsyncEmailValidatorDirective,
+      multi: true,
+    },
+  ],
 })
 export class AsyncEmailValidatorDirective {
-
-  constructor() { }
 
 }
