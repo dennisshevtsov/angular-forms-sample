14fabe81bb4bc81adbd0eca3b327814b510a302b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-07-12 16:54:27 +0300

    valid

--- src/app/validators/custom.validators.ts
+++ src/app/validators/custom.validators.ts
@@ -32,6 +32,24 @@ export class CustomValidators {
       }, 2000);
     });
   }
+
+  public static emailMatcher(c: AbstractControl):
+    ValidationErrors | null {
+    const emailControl = c.get('email');
+    const confirmEmailControl = c.get('confirmEmail');
+
+    if (emailControl?.pristine || confirmEmailControl?.pristine) {
+      return null;
+    }
+
+    if (emailControl?.value === confirmEmailControl?.value) {
+      return null;
+    }
+
+    return {
+      emailMatch: true,
+    };
+  }
 }
 
 export function checkServiceLevel(
